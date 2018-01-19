7e68a75c8d22142dde04686bf8dd0a66ef3d5a6a
Madhusudhan Reddy K <madhusudhan.reddy@quikr.com>
2018-01-19 15:18:28 +0530

    Changed mysql db to sqllite

--- PseudoStockArchive/settings.py
+++ PseudoStockArchive/settings.py
@@ -79,12 +79,8 @@ WSGI_APPLICATION = 'PseudoStockArchive.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'test',
-        'USER': 'test',
-        'PASSWORD': 'asdfghjk',
-        'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'sqlitedb'),
     }
 }
 
